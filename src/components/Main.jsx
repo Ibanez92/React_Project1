021e368519a3633f8fab2b755dd3503f8c2713e2
Javier Ibanez <Expertprep2015@gmail.com>
2023-02-26 21:52:04 -0500

    adding css styling to my Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 export default function Main() {
     return (
-        <div>
-            <h1>main component</h1>
+        <div className="main">
+            <h1>Fun facts about React</h1>
+            <ol className="main-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Is maintained by Meta</li>
+            </ol>
         </div>
     )
 }
