d7a249ba571962073b0f84e947b5ba178d9d3bf7
Javier Ibanez <Expertprep2015@gmail.com>
2023-02-26 21:58:17 -0500

    changed my ol to a ul in my Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,11 @@ export default function Main() {
     return (
         <div className="main">
             <h1>Fun facts about React</h1>
-            <ol className="main-list">
+            <ul className="main-list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Is maintained by Meta</li>
-            </ol>
+            </ul>
         </div>
     )
 }
