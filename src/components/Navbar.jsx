acca97f5999a0da801a94e5535e1e620e2251aff
Javier Ibanez <Expertprep2015@gmail.com>
2023-02-26 21:03:47 -0500

    removed unnecessary css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,7 @@ export default function Navbar() {
             <nav className="nav">
                 <img className="nav-img" src="./Images/react-icon-small.png" alt="reactlogo"/>
                 <h3>ReactFacts</h3>
-                <h4>React Project 1
-                </h4>
+                <h4>React Project 1</h4>
             </nav>
         </header>
     )
